bca5038ddc400f4f89787bdfd72781ee6b866171
rushia <jonasg.13452@gmail.com>
2022-03-27 10:56:21 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ const WIDTH: f32 = 800.;
 const HEIGHT: f32 = 800.;
 const FPS: u8 = 60;
 
-#[allow(dead_code, unused_variables)]
+#[derive(Copy, Clone)]
 struct OrbitalBody {
     pos: (f32, f32),
     mass: f32,
@@ -45,22 +45,34 @@ impl Position for OrbitalBody {
 }
 
 struct Simulation {
+    items: Vec<OrbitalBody>,
     qt: QuadTree<OrbitalBody>,
 }
 
 impl Simulation {
-    fn new(_ctx: &mut Context, qt: QuadTree<OrbitalBody>) -> Self {
-        Simulation{ qt }
+    fn new(_ctx: &mut Context, items: Vec<OrbitalBody>, qt: QuadTree<OrbitalBody>) -> Self {
+        Simulation{ items, qt }
     }
 }
 
 impl EventHandler for Simulation {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        for i in &mut self.items {
+            let mut new_x = i.pos.0 + i.vel * i.ang.cos();
+            let mut new_y = i.pos.1 + i.vel * i.ang.sin();
+            if new_x > WIDTH || new_x < 0. {
+                new_x = max(0., min(WIDTH, new_x));
+            }
+            if new_y > HEIGHT || new_y < 0. {
+                new_y = max(0., min(HEIGHT, new_y));
+            }
+            i.pos = (new_x, new_y);
+        }
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, Color::BLACK);
-        for i in self.qt.query_all() {
+        for i in &self.items {
             let circle = graphics::Mesh::new_circle(
                 ctx,
                 graphics::DrawMode::fill(),
@@ -77,6 +89,7 @@ impl EventHandler for Simulation {
        
 #[allow(unused_variables, unused_mut)]
 fn main() {
+
     let (mut ctx, event_loop) = ContextBuilder::new("Simulation", "")
         .window_setup(WindowSetup::default().title("Simulation"))
         .window_mode(WindowMode::default().dimensions(WIDTH, HEIGHT))
@@ -89,9 +102,17 @@ fn main() {
                                 (WIDTH / 2.).into(), 
                                 (HEIGHT / 2.).into()
                                     ));
+
     let o = OrbitalBody::new((400., 400.), 5., 0.);
     let r = OrbitalBody::new((200., 100.), 2., 0.);
-    qt.insert_all(vec![o, r]);
-    let simulation = Simulation::new(&mut ctx, qt);
+    let mut simulation = Simulation::new(&mut ctx, vec![o.clone(), r.clone()], qt);
+    simulation.qt.insert_all(vec![o, r]);
     event::run(ctx, event_loop, simulation);
+}
+
+fn max(a: f32, b: f32) -> f32 {
+    if a > b { a } else { b }
+}
+fn min(a: f32, b: f32) -> f32 {
+    if a < b { a } else { b }
 }
